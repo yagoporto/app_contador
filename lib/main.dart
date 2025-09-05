717df51e1032ac54c26779aaa96c886504e7af8a
Yago Porto <yagosouz8@gmail.com>
2025-09-04 21:52:21 -0300

    Criação das logicas dos botões e mudança no estilo

--- lib/main.dart
+++ lib/main.dart
@@ -9,92 +9,132 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int count = 0;
+
   void decrement(){
-    print('Decrement');
+    setState(() {
+    count--;
+    });
+    print(count);
   }
 
   void increment(){
-    print('Increment');
+    setState(() {
+    count++;
+    });
+    print(count);
   }
+
+  bool get isEmpty => count == 0;
+  bool get isFull => count >= 10;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 216, 216, 216),
-      body: Column(
-        mainAxisAlignment:
-            MainAxisAlignment.center,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover,
+          )
 
-        children: [
-          const Text(
-            'Pode entrar!',
-            style: TextStyle(
-              fontSize: 26,
-              color: Colors.deepOrangeAccent,
-              fontWeight: FontWeight.w700,
+        ), 
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.center,
+        
+          children: [
+            Text(
+              isFull ?'Lotado!' : 'Pode entrar!',
+              style: TextStyle(
+                fontSize: 26,
+                color: Colors.deepOrangeAccent,
+                fontWeight: FontWeight.w700,
+              ),
             ),
-          ),
-          
+            //alert
+            Padding(
+              padding: EdgeInsetsGeometry.fromLTRB(32, 32, 32, 8),
+              child: Container(
+                  width: 35,
+                  height: 35,
+                  decoration:  BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/gif/14.gif'),
+                      fit: BoxFit.cover,
+                      opacity: isFull ? 1.0 : 0.0, 
+                    ),
+                  ),
+                ), 
+            ),
 
-          const Padding(
-            padding: EdgeInsetsGeometry.all(32),
-            child: Text(
-            '0',
-            style: TextStyle(
-              fontSize: 35,
-            ),
-          ), 
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-            TextButton(
-              
-              onPressed: decrement,
+            //contador
+            Padding(
+              padding: EdgeInsets.fromLTRB(8, 8, 8, 32),
+                child: Text(
+                  '$count',
+                    style: TextStyle(
+                    fontSize: 35,
+                    color: isFull ? Colors.red :  Colors.white,
+                    ),
+                  ),
+              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              TextButton(
+                onPressed: isEmpty ? null : decrement,
+                style: TextButton.styleFrom(
+                  backgroundColor: isEmpty ? Colors.white.withOpacity(0.2) : Colors.white,
+                  fixedSize: const Size(100, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(8),
+                  )
+        
+                ),
+                child: Text('Saiu', 
+                style:  TextStyle(color:Colors.deepOrange,
+                        fontSize: 20,),
+                      ),
+              ),
+              const SizedBox(width: 32,),
+              TextButton(
+              onPressed: isFull ? null : increment,
               style: TextButton.styleFrom(
-                backgroundColor: Colors.white,
+                backgroundColor: isFull ? Colors.white.withOpacity(0.2) : Colors.white,
                 fixedSize: const Size(100, 50),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadiusGeometry.circular(8),
-                )
-
+                  )
               ),
-              child: Text('Saiu', 
-              style:  TextStyle(color:Colors.deepOrange,
-                      fontSize: 20,),
-                    ),
-            ),
-            const SizedBox(width: 32,),
-            TextButton(
-            onPressed: increment,
-            style: TextButton.styleFrom(
-              backgroundColor: Colors.white,
-              fixedSize: const Size(100, 50),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadiusGeometry.circular(8),
-                )
-            ),
-            child: Text('Entrou',
-            style:  TextStyle(
-              color:Colors.deepOrange,
-              fontSize: 20,
+              child: Text('Entrou',
+              style:  TextStyle(
+                color:Colors.deepOrange,
+                fontSize: 20,
+                ),
               ),
-            ),
-            )
-          ],),
-
-        ],
+              )
+            ],),
+          ],
+        ),
       ),
     );
   }
-}
-
+  }
